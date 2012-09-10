5891e60ef9acdac642583a1dd2eef205c88d8982
John Casey <jdcasey@commonjava.org>
2012-09-10 12:11:32 -0500

    Bootstrapping project to validate Maven repositories

---
+++ src/main/java/org/commonjava/redhat/maven/rv/App.java
@@ -0,0 +1,13 @@
+package org.commonjava.redhat.maven.rv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
